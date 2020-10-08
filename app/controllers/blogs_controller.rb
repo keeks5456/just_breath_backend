91b9fbdb03e5ee8eb72fb96a2ab9150eae8f6c65
keeks5456 <Akikogreen08@gmail.com>
2020-10-08 09:45:00 -0700

    blogs and journal

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,50 @@
 class BlogsController < ApplicationController
     def index 
-        blog = Blogs.all 
-      
+        blogs = Blogs.all 
+        render json: blogs 
+    end
+
+    def show 
+        blog = Blog.find_by(:id params[:id])
+        if blog
+            render json: blog 
+        else
+            render json: { message: 'This Blog does not exist' }
+    end
+
+    def create 
+        blog = Blog.create(blog_params)
+        if blog.valid?
+            render json: blog 
+        else
+            render json: {error: 'failed to create blog'} 
+    end
+
+    def update
+        # blog = Blog.find_by(id: params[:id])
+        find_blog
+        if blog.update(blog_params)
+          render json: blog
+        else
+          render json: { error: 'Something went wrong' }
+        end
+      end
+
+      def destroy
+        # blog = Blog.find_by(id: params[:id])
+        find_blog
+        user.destroy
+    
+        render json: { message: 'deleted' }
+      end
+
+    private
+
+    def find_blog
+        blog = Blog.find_by(id: params[:id])
+    end
+
+    def blog_params
+        params.require(:blog).permit(:img_url, :description)
     end
 end
