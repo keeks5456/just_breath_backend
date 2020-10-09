922087e29d267e95fd1fc3a8956e5c5acf44e056
keeks5456 <Akikogreen08@gmail.com>
2020-10-08 22:09:06 -0700

    add dependnt destroy on models

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,4 +1,4 @@
 class Exercise < ApplicationRecord
-    has_many :user_favorite_exercises
-    has_many :users, through: :user_favorite_exercises
+    has_many :user_favorite_exercises, dependent: :destroy
+    has_many :users, through: :user_favorite_exercises, dependent: :destroy
 end
