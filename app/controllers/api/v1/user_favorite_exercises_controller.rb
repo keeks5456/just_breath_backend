bca9abb053aa1cd5c7dfcdfbfd7fe2c8cee70d19
keeks5456 <Akikogreen08@gmail.com>
2020-10-19 22:21:20 -0700

    favorite blogs controller create action created

--- app/controllers/api/v1/user_favorite_exercises_controller.rb
+++ app/controllers/api/v1/user_favorite_exercises_controller.rb
@@ -8,6 +8,15 @@ class Api::V1::UserFavoriteExercisesController < ApplicationController
         render json: user_favorite_exercises
     end
 
+    def create 
+        user_favorite_exercises = UserFavoriteExercise.create(user_favorite_exercises_params)
+        if user_favorite_exercises.valid?
+            render json: user_favorite_exercises 
+        else
+            render json: {error: 'failed to create blog'} 
+        end
+    end
+
     def update
         user_favorite_exercises = user_favorite_exercises_params
         if user_favorite_exercises.update(find_user_favorites_exercises)
